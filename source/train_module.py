32df338c692a94afa86a27dd1cdb7f4986c05935
jiwon <pythonzz0622@gmail.com>
2022-07-24 23:08:06 +0900

    v4

--- source/train_module.py
+++ source/train_module.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 import warnings
 warnings.filterwarnings(action='ignore')
 import utils
-
+from sklearn.metrics import roc_auc_score
+tf.config.run_functions_eagerly(True)
 class training():
     '''
     training 시켜주는 모듈
@@ -22,7 +23,8 @@ class training():
         gradients = tape.gradient(loss, self.model.trainable_weights)
         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_weights))
         correct = (tf.math.argmax(pred , axis = 1 ) == labels)
-        return loss , correct
+        auc = roc_auc_score(labels, pred.numpy()[:, 1] )
+        return loss , correct , auc 
 
     @tf.function
     def _val_step(self , images, labels):
@@ -30,66 +32,80 @@ class training():
         pred = self.model(images, training=False)
         loss = self.loss_fn(labels, pred)
         correct = (tf.math.argmax(pred , axis = 1 ) == labels)
-        return loss , correct
+        auc = roc_auc_score(labels, pred.numpy()[:, 1] )
+        return loss , correct , auc
 
     def training(self , train_generator ,val_generator  , epoch ):
         best_val_acc = 0.0
         best_train_acc = 0.0
+        best_val_auc = 0.0
         train_loss_list = []
         train_acc_list = []
         val_loss_list = []
         val_acc_list = []
+        
 
         for epoch in tqdm.tqdm(range(1 ,epoch)):
 
             size = 0
             train_loss = 0
             train_acc = 0
+            train_auc = 0.0
 
             for i, (images, labels) in enumerate(train_generator):
-                loss , correct = self._train_step(images, labels)
+                loss , correct , auc  = self._train_step(images, labels)
 
                 size += labels.shape[0]
                 train_loss += loss.numpy().sum()
                 train_acc += correct.numpy().sum()
+                train_auc += auc
 
             train_loss /= (i+1)
             train_acc /= size
+            train_auc /= (i + 1)
 
             size = 0
             val_loss = 0
             val_acc = 0
+            val_auc = 0.0
             for j ,(images, labels) in enumerate(val_generator):
-                loss , correct = self._val_step(images, labels)
+                loss , correct , auc = self._val_step(images, labels)
 
                 size += labels.shape[0]
                 val_loss += loss.numpy().sum()
                 val_acc += correct.numpy().sum()
+                val_auc += auc
 
             val_loss /= (j + 1)
+            val_auc /= (j + 1)
             val_acc /= size
+
             if epoch % 50 == 0 :
                 template = 'epoch: {} , loss: {:.3f} , acc: {:.3f} , val_loss: {:.3f} , val_acc : {:.3f}'
                 print(template.format(epoch + 1, train_loss, train_acc * 100, val_loss, val_acc * 100))
 
-            if val_acc > best_val_acc:
+            if val_auc > best_val_auc:
                 best_train_acc = train_acc
                 best_train_loss = train_loss
+                best_train_auc = train_auc
+                best_val_auc = val_auc
                 best_val_acc = val_acc
                 best_val_loss = val_loss
-                best_metrics = {'train_acc' : best_train_acc  , 'val_acc' : best_val_acc , 'train_loss' : best_train_loss , 'val_loss' : best_val_loss}
+
+                best_metrics = {'train_acc' : best_train_acc  , 'val_acc' : best_val_acc , 'train_loss' : best_train_loss , 'val_loss' : best_val_loss , 
+                                'train_auc' : best_train_auc  , 'val_auc' : best_val_auc}
                 best_model = self.model
                 # best_model.save_weights(os.path.join('../models', 'DRD_{}.h5'.format(epoch)))
-
             train_loss_list.append(train_loss)
             train_acc_list.append(train_acc)
             val_loss_list.append(val_loss)
             val_acc_list.append(val_acc)
             
         print('end')
-        metrics = {'train_loss' : train_loss_list , 'train_acc' : train_acc_list, 
-                'val_acc' : val_acc_list , 'val_loss' : val_loss_list  }
-
+        metrics = {'train_loss' : train_loss_list , 'train_acc' : train_acc_list,  
+                'val_acc' : val_acc_list , 'val_loss' : val_loss_list   }
+        print(best_metrics)
+        print(best_model)
         utils.save_plot(metrics , 'epoch200' , dir = './artifact/')
 
         return best_model , best_metrics
--- tuning/train_script.sh
+++ tuning/train_script.sh
@@ -1,5 +1,4 @@
 #!bin/sh
-
 : <<'END'
 python train_v2.py --optimizer "SGD" --lr 0.1 --addtag "set:optim"
 python train_v2.py --optimizer "SGD" --lr 0.01 --addtag "set:optim"
@@ -8,16 +7,14 @@ python train_v2.py --optimizer "SGD" --lr 0.001 --addtag "set:optim"
 python train_v2.py --optimizer "RMSProp" --lr 0.1 --addtag "set:optim"
 python train_v2.py --optimizer "RMSProp" --lr 0.01 --addtag "set:optim"
 python train_v2.py --optimizer "RMSProp" --lr 0.001 --addtag "set:optim"
-END
-
-echo StartRun
+ß
 python train_v2.py --optimizer "Adam" --lr 0.1 --addtag "set:optim"
 python train_v2.py --optimizer "Adam" --lr 0.01 --addtag "set:optim"
 python train_v2.py --optimizer "Adam" --lr 0.001 --addtag "set:optim"
-
-python train_v2.py --optimizer "Nadam" --lr 0.1 --addtag "set:optim"
-python train_v2.py --optimizer "Nadam" --lr 0.01 --addtag "set:optim"
-python train_v2.py --optimizer "Nadam" --lr 0.001 --addtag "set:optim"
+END
+python train_v2.py --optimizer "NAdam" --lr 0.1 --addtag "set:optim"
+python train_v2.py --optimizer "NAdam" --lr 0.01 --addtag "set:optim"
+python train_v2.py --optimizer "NAdam" --lr 0.001 --addtag "set:optim"
 
 python train_v2.py --optimizer "Adamax" --lr 0.1 --addtag "set:optim"
 python train_v2.py --optimizer "Adamax" --lr 0.01 --addtag "set:optim"
